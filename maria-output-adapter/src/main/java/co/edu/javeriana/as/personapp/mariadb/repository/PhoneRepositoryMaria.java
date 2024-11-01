786feab669ec5b4f4502d8639e3f6fe20bd768d8
Nicolas Quintana <140460204+Nikoquin1@users.noreply.github.com>
2024-10-31 21:25:34 -0500

    me falto este

---
+++ maria-output-adapter/src/main/java/co/edu/javeriana/as/personapp/mariadb/repository/PhoneRepositoryMaria.java
@@ -0,0 +1,10 @@
+package co.edu.javeriana.as.personapp.mariadb.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import co.edu.javeriana.as.personapp.mariadb.entity.TelefonoEntity;
+
+public interface PhoneRepositoryMaria extends JpaRepository<TelefonoEntity, String>{
+
+}
